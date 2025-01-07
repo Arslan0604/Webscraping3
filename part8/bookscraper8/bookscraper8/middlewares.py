1489c621e23af02c3f9c84b2e826378d8275807e
Arslan Ovezov <macbookpro@MacBooks-MacBook-Pro.local>
2025-01-07 20:07:49 +0500

    solved

--- part8/bookscraper8/bookscraper8/middlewares.py
+++ part8/bookscraper8/bookscraper8/middlewares.py
@@ -187,7 +187,7 @@ class ScrapeOpsFakeBrowserHeaderAgentMiddleware:
         request.headers['accept-language'] = random_browser_header['accept-language']
         request.headers['sec-fetch-user'] = random_browser_header['sec-fetch-user']
         request.headers['sec-fetch-site'] = random_browser_header['sec-fetch-site']
-        request.headers['sec-ch-ua-platform'] = random_browser_header['sec-ch-ua-platform']
+        
         request.headers['sec-ch-ua-mobile'] = random_browser_header['sec-ch-ua-mobile']
         request.headers['sec-ch-ua'] = random_browser_header['sec-ch-ua']
         request.headers['accept'] = random_browser_header['accept']
