1644c9806facfb62f273b69e17a5981d852c0b22
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-08 18:21:04 +0500

    arslan done

--- part8/bookscraper8/bookscraper8/middlewares.py
+++ part8/bookscraper8/bookscraper8/middlewares.py
@@ -183,16 +183,27 @@ class ScrapeOpsFakeBrowserHeaderAgentMiddleware:
 
     def process_request(self, request, spider):        
         random_browser_header = self._get_random_browser_header()
+
+        request.headers['accept-language'] = random_browser_header.get('accept-language', '')
+        request.headers['sec-fetch-user'] = random_browser_header.get('sec-fetch-user', '')
+        request.headers['sec-fetch-mode'] = random_browser_header.get('sec-fetch-mode', '')
+        request.headers['sec-fetch-site'] = random_browser_header.get('sec-fetch-site', '')
+        request.headers['sec-ch-ua-platform'] = random_browser_header.get('sec-ch-ua-platform', '')
+        request.headers['sec-ch-ua-mobile'] = random_browser_header.get('sec-ch-ua-mobile', '')
+        request.headers['sec-ch-ua'] = random_browser_header.get('sec-ch-ua', '')
+        request.headers['accept'] = random_browser_header.get('accept', '')
+        request.headers['user-agent'] = random_browser_header.get('user-agent', '')
+        request.headers['upgrade-insecure-requests'] = random_browser_header.get('upgrade-insecure-requests', '')
         
-        request.headers['accept-language'] = random_browser_header['accept-language']
-        request.headers['sec-fetch-user'] = random_browser_header['sec-fetch-user']
-        request.headers['sec-fetch-mode'] = random_browser_header['sec-fetch-mode']
-        request.headers['sec-fetch-site'] = random_browser_header['sec-fetch-site']
-        request.headers['sec-ch-ua-platform'] = random_browser_header['sec-ch-ua-platform']
-        request.headers['sec-ch-ua-mobile'] = random_browser_header['sec-ch-ua-mobile']
-        request.headers['sec-ch-ua'] = random_browser_header['sec-ch-ua']
-        request.headers['accept'] = random_browser_header['accept']
-        request.headers['user-agent'] = random_browser_header['user-agent']
-        request.headers['upgrade-insecure-requests'] = random_browser_header['upgrade-insecure-requests']
+        #request.headers['accept-language'] = random_browser_header['accept-language']
+        #request.headers['sec-fetch-user'] = random_browser_header['sec-fetch-user']
+        #request.headers['sec-fetch-mode'] = random_browser_header['sec-fetch-mode']
+        #request.headers['sec-fetch-site'] = random_browser_header['sec-fetch-site']
+        #request.headers['sec-ch-ua-platform'] = random_browser_header['sec-ch-ua-platform']
+        #request.headers['sec-ch-ua-mobile'] = random_browser_header['sec-ch-ua-mobile']
+        #request.headers['sec-ch-ua'] = random_browser_header['sec-ch-ua']
+        #request.headers['accept'] = random_browser_header['accept']
+        #request.headers['user-agent'] = random_browser_header['user-agent']
+        #request.headers['upgrade-insecure-requests'] = random_browser_header['upgrade-insecure-requests']
 
 
