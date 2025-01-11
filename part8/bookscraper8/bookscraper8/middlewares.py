2a34316c54402d8903278f72b89413c8db1744d6
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-11 19:35:00 +0500

    do not under

--- part8/bookscraper8/bookscraper8/middlewares.py
+++ part8/bookscraper8/bookscraper8/middlewares.py
@@ -3,10 +3,10 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-from scrapy import signals
+from scrapy import signals # type: ignore
 
 # useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
+from itemadapter import is_item, ItemAdapter # type: ignore
 
 
 class Bookscraper8SpiderMiddleware:
