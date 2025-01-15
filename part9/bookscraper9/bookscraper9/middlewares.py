c89bc1871f560c1e65ba2fd74376181465f63f65
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-15 23:33:42 +0500

    done

--- part9/bookscraper9/bookscraper9/middlewares.py
+++ part9/bookscraper9/bookscraper9/middlewares.py
@@ -9,8 +9,6 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-
-
 class Bookscraper9SpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
@@ -208,7 +206,7 @@ import base64
 class MyProxyMiddleware(object):
 
     @classmethod
-    def __init__(cls, crawler):
+    def from_crawler(cls, crawler):
         return cls(crawler.settings) 
     
     def __init__(self, settings):
@@ -219,7 +217,7 @@ class MyProxyMiddleware(object):
 
     def process_request(self, request, spider):
         user_credentials = '{user}:{passw}'.format(user=self.user, passw=self.password)
-        basic_authentication = 'Basic ' + base64.b64decode(user_credentials.encode()).decode()
+        basic_authentication = 'Basic ' + base64.b64encode(user_credentials.encode()).decode()
         host = 'http://{endpoint}:{port}'.format(endpoint=self.endpoint, port=self.port)
         request.meta['proxy'] = host
         request.headers['Proxy-Authorization'] = basic_authentication
